9caaf86c0dc382a1c4d7ecf388a5d15dc02d5c48
kurrx <kurr.eax@gmail.com>
2024-03-10 18:36:50 +0500

    feat: added video related setting for `tv.kurr.dev`

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 import createServer from './src/index.js'
+import { rewriteCookies } from './src/utils.js'
 import 'dotenv/config'
 
+const TV_PROVIDER_HOST = process.env.TV_PROVIDER_HOST || null
 const server = createServer({
   proxyHttps: process.env.NODE_ENV === 'production',
   originWhitelist: process.env.ORIGIN_WHITELIST?.split(', ') || [],
   originBlacklist: process.env.ORIGIN_BLACKLIST?.split(', ') || [],
+  removeHeaders: ['cookie', 'origin', 'referer'],
+  handleInitialRequest(req) {
+    const location = req.proxyState.location
+    if (!location) return false
+    req.proxyState.videoName = location.searchParams.get('proxy-tv-video-filename')
+    return false
+  },
+  isEmptyOriginAllowed(req) {
+    const location = req.proxyState.location
+    if (!location || !TV_PROVIDER_HOST) return false
+    return location.hostname.endsWith(TV_PROVIDER_HOST) && location.pathname.endsWith('.mp4')
+  },
+  handleResponse(req, res, proxyReq, proxyRes) {
+    rewriteCookies(proxyRes.headers)
+    const contentType = proxyRes.headers['content-type'] || proxyRes.headers['Content-Type']
+    if (contentType === 'video/mp4' && proxyReq.method === 'GET' && req.proxyState.videoName) {
+      proxyRes.headers['Content-Disposition'] = `attachment; filename="${req.proxyState.videoName}"`
+    }
+    return false
+  },
+  isAllowedToFollowRedirect(req) {
+    const location = req.proxyState.location
+    if (!location || !TV_PROVIDER_HOST) return false
+    return !location.hostname.endsWith(TV_PROVIDER_HOST)
+  },
 })
 
 const PORT = process.env.PORT || 4001
